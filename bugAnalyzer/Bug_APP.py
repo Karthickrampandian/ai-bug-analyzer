de86705ce25499cff8b281a61e5b6129233de985
Karthickrampandian <karthick.ramap007@gmail.com>
2026-04-11 08:36:40 +0800

    Removed unwanted text

--- bugAnalyzer/Bug_APP.py
+++ bugAnalyzer/Bug_APP.py
@@ -48,12 +48,6 @@ class bug_app:
                 bugtable = pd.DataFrame(rows)
                 st.session_state["bugtable"] = bugtable
 
-                # csv = bugtable.to_csv(index=False)
-                # st.download_button(
-                #     label="Download Bug Report",
-                #     data=csv,
-                #     file_name=f"bug_report_{bug_input}.csv",
-                #     mime="text/csv")
             else:
                 st.write("Error:", solution)
 
